06b6d5c7a9af1be45c550ca86f59538462d7f107
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-17 21:51:08 -0300

    feat: update App to use Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
-import Home from './components/Home/Home';
-
-
+import About from './views/About/About';
+import Contact from './views/Contact/Contact';
+import Home from './views/Home/Home';
+import Services from './views/Services/Services';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<About />} />
+        <Route path="/contato" element={<Contact />} />
+        <Route path="/servicos" element={<Services />} />
       </Routes>
     </div>
   );
