855f3ad73fad078d202a91d1624d4b6ed837f4c5
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-18 14:58:06 -0300

    feat: add Headline component

---
+++ frontend/src/components/Headline/Headline.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Headline.css';
+
+import device from '../../static/images/device.png';
+
+function Headline () {
+  return (
+    <section>
+      <div className="columns">
+        <div className="column is-half is-offset-one-quarter">
+          <div className="is-flex is-justify-content-space-between is-align-items-center">
+            <div className="headline-main has-text-light">
+              <h2 className="is-capitalized is-size-4">Desenvolvemos sistemas únicos</h2>
+              <p className="my-2">
+                Somos especialistas no desenvolvimento de
+                <ul>
+                  <li>Sites</li>
+                  <li>Lojas virtuais</li>
+                  <li>ERP's</li>
+                </ul>
+              </p>
+              <Link to="/contato">
+                <button className="button is-warning mb-2">Entre em contato</button>
+              </Link>
+            </div>
+            <div className="headline-image  is-hidden-touch">
+              <img src={ device } alt="Device responsiviness" title="Implementando sistemas Responsivos" aria-hidden="true" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Headline;
--- frontend/src/views/Home/Home.jsx
+++ frontend/src/views/Home/Home.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './Home.css';
 
 import Banner from '../../components/Banner/Banner';
+import Headline from '../../components/Headline/Headline';
 import Footer from '../../components/Footer/Footer';
 
 function Home () {
   return (
     <div className="home">
       <Banner />
+      <Headline />
       <Footer />
     </div>
   );
