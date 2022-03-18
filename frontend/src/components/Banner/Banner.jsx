bdd4d8983c8455f54c26d4522c8cb35ac062c96a
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-17 22:38:55 -0300

    feat: finish banner component

--- frontend/src/components/Banner/Banner.jsx
+++ frontend/src/components/Banner/Banner.jsx
@@ -9,6 +9,16 @@ function Banner () {
     <div className="banner">
       <header>
         <Navbar />
+        <div className="head">
+          <div className="columns is-mobile is-centered m-4">
+            <div className="column is-5-desktop is-12-mobile has-text-light">
+              <h2 className="is-size-2">Sistemas próprios para empresas autênticas</h2>
+              <p>
+                Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+              </p>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
