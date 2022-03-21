cdf4cc0f582e1e2edb489bec3aca72ad5e9dde48
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-20 21:44:11 -0300

    refactor: update members card

--- frontend/src/components/Team/Team.jsx
+++ frontend/src/components/Team/Team.jsx
@@ -4,15 +4,15 @@ import './Team.css';
 
 function Team () {
   return (
-    <section>
+    <section className="mb-4">
       <div className="container mb-4 pb-4">
         <h2 className="is-size-4 has-text-centered my-4">Desenvolvido de pessoas para pessoas</h2>
         <p className="has-text-centered">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, incidunt praesentium fugiat placeat corporis adipisci. Obcaecati temporibus, autem libero facilis sapiente ex! Atque repellendus temporibus, tenetur ducimus quae iste laboriosam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima incidunt temporibus, iure ullam amet laborum reprehenderit explicabo natus ut porro dolorum odio quia id consectetur delectus. Iusto distinctio assumenda repudiandae.
         </p>
       </div>
-      <div className="members columns mb-4">
-        <div className="column is-6">
+      <div className="members columns is-mobile is-multiline is-centered">
+        <div className="column is-narrow">
           <div class="card">
             <div class="card-content">
               <div class="media">
@@ -37,13 +37,13 @@ function Team () {
             </div>
           </div>
         </div>
-        <div className="column is-6">
+        <div className="column is-narrow">
           <div class="card">
             <div class="card-content">
               <div class="media">
                 <div class="media-left">
                   <figure class="image is-48x48">
-                    <img src="https://media-exp1.licdn.com/dms/image/C5635AQFkxx2nDqnDlw/profile-framedphoto-shrink_800_800/0/1614697137195?e=1647720000&v=beta&t=CwGXaNUJdGZPmNRusj5QBKD51dykppe-wG1hfgrfEvA" alt="Placeholder image" />
+                    <img src="https://media-exp1.licdn.com/dms/image/C5603AQECQwv4g9LohQ/profile-displayphoto-shrink_800_800/0/1643858429793?e=1652918400&v=beta&t=xA5QjiTnnPjSdW95L_4Z4o_hNjCmHa7AynwdniGoPdo" alt="Placeholder image" />
                   </figure>
                 </div>
                 <div class="media-content">
