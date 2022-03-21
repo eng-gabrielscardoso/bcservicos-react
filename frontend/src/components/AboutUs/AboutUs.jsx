254d66e4b4589c9a3cd74b5c90f42c12bfba7b77
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-21 15:40:15 -0300

    feat: add AboutUs component

---
+++ frontend/src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './AboutUs.css';
+
+function AboutUs () {
+  return (
+    <section className="about-us is-flex is-justify-content-center">
+      <div className="headline-section">
+        <h2 className="is-size-4 my-4 has-text-centered has-text-link">Somos uma empresa multi <br /> <i className="fa fa-building has-text-link"></i></h2>
+        <p className="is-half has-text-centered my-4">
+          Multidisciplinar. Multiétnica. Multifacetada
+          <br />
+          Estamos comprometidos a, dia após dia, fornecer o melhor em nossos serviços para nossos clientes.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore at earum modi. Porro commodi laudantium veritatis nostrum. Saepe repudiandae nam a magni ducimus, quisquam nostrum ullam, omnis ad dicta labore. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore at earum modi. Porro commodi laudantium veritatis nostrum. Saepe repudiandae nam a magni ducimus, quisquam nostrum ullam, omnis ad dicta labore. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore at earum modi. Porro commodi laudantium veritatis nostrum. Saepe repudiandae nam a magni ducimus, quisquam nostrum ullam, omnis ad dicta labore.
+        </p>
+      </div>
+      <div className="history-section">
+      <h2 className="is-size-4 my-4 has-text-centered has-text-link">Nossa história <br /> <i className="fa fa-book has-text-link"></i></h2>
+        <p className="is-half has-text-centered my-4">
+          Estamos comprometidos a, dia após dia, fornecer o melhor em nossos serviços para nossos clientes.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore at earum modi. Porro commodi laudantium veritatis nostrum. Saepe repudiandae nam a magni ducimus, quisquam nostrum ullam, omnis ad dicta labore. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore at earum modi. Porro commodi laudantium veritatis nostrum. Saepe repudiandae nam a magni ducimus, quisquam nostrum ullam, omnis ad dicta labore. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore at earum modi. Porro commodi laudantium veritatis nostrum. Saepe repudiandae nam a magni ducimus, quisquam nostrum ullam, omnis ad dicta labore.
+        </p>
+      </div>
+      <h2 className="is-size-4 my-4 has-text-centered has-text-link">Missão, visão, valores <br /> <i className="fa fa-bullseye has-text-link"></i></h2>
+        <p className="is-half has-text-centered my-4">
+          Estamos comprometidos a, dia após dia, fornecer o melhor em nossos serviços para nossos clientes.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore at earum modi. Porro commodi laudantium veritatis nostrum. Saepe repudiandae nam a magni ducimus, quisquam nostrum ullam, omnis ad dicta labore. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore at earum modi. Porro commodi laudantium veritatis nostrum. Saepe repudiandae nam a magni ducimus, quisquam nostrum ullam, omnis ad dicta labore. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore at earum modi. Porro commodi laudantium veritatis nostrum. Saepe repudiandae nam a magni ducimus, quisquam nostrum ullam, omnis ad dicta labore.
+        </p>
+    </section>
+  );
+}
+
+export default AboutUs;
--- frontend/src/views/About/About.jsx
+++ frontend/src/views/About/About.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 import './About.css';
 
+import Banner from '../../components/Banner/Banner';
+import AboutUs from '../../components/AboutUs/AboutUs';
+import Footer from '../../components/Footer/Footer';
+
 function About () {
   return (
     <div className='about'>
-      Sobre
+      <Banner mainTitle="Somos mais que uma empresa, somos uma filosofia" subTitle="A BC Serviços acredita não somente na relação de empresa, mas na relação de facilitadores para seus clientes. Empenhada sempre em aliviar as necessidades e fornecer novas soluções aos seus empregadores." />
+      <AboutUs />
+      <Footer />
     </div>
   );
 }
